9f3b171e39d5f9a8ca55d33be0665bcfbb8e3444
jr-k <jessym.reziga@gmail.com>
2023-09-04 21:45:26 +0200

    new categories

--- src/categories.js
+++ src/categories.js
@@ -2,12 +2,15 @@ const categories = {
   "meat": { name: "Boucherie", icon: "🥩", id: "meat" },
   "fish": { name: "Poissonerie", icon: "🐟", id: "fish" },
   "milk": { name: "Laitages", icon: "🧀", id: "milk" },
+  "fruits": { name: "Fruits", icon: "🍓", id: "fruits" },
+  "vegetables": { name: "Légumes", icon: "🥦", id: "vegetables" },
   "sweet": { name: "Sucré", icon: "🍪", id: "sweet" },
   "salty": { name: "Salé", icon: "🥨", id: "salty" },
+  "bread": { name: "Boulangerie", icon: "🥐", id: "bread" },
   "body": { name: "Hygiène corporelle", icon: "🚿", id: "body" },
   "cleaning": { name: "Entretien maison", icon: "🧽", id: "cleaning" },
   "frozen": { name: "Surgelé", icon: "❄️", id: "frozen" },
-  "bread": { name: "Boulangerie", icon: "🥐", id: "bread" }
+  "others": { name: "Autres", icon: "🪣", id: "others" }
 };
 
 export default categories;
